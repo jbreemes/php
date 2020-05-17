76a8e2945e84f60e9ec33783e2209b2c6d57ef94
jozua <jbreemes@roc-dev.com>
2020-05-17 13:35:33 +0200

    php

--- Opdarcht 3/Opdracht 1.5.php
+++ Opdarcht 3/Opdracht 1.5.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>PHP</title>
     <style>
+        body{
+            text-align: left;
+        }
 
     </style>
 </head>
@@ -16,10 +19,10 @@ $swimclub = array("De Spartelkuikens" => "25", "De Waterbuffels" => "32", "Plons
 $plaatje = 0;
 
 foreach ($swimclub as $club => $leden) {
-    $afgerondLedenAantal = floor($leden/5);
+    $AantalLeden = floor($leden/5);
     echo $club. " ". $leden;
-    for ($plaatje = 0; $plaatje < $afgerondLedenAantal; $plaatje++) {
-        echo "<img src='img/zwem.png'>";
+    for ($plaatje = 0; $plaatje < $AantalLeden; $plaatje++) {
+        echo " <img src='img/zwem.png'>";
     }
     echo "<br>";
 }
