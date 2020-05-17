c92e4de3ea62355bc4950884eb0d0e318ba3ed1a
jozua <jbreemes@roc-dev.com>
2020-05-17 19:17:37 +0200

    Opdracht 1.6 niet vergeten

---
+++ Opdracht 5/Opdracht 3.3.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+    <style>
+        body{
+            text-align: -moz-center;
+        }
+
+    </style>
+</head>
+<body>
+<FORM NAME ="site" METHOD ="POST" ACTION = "Opdracht%203.3.php" >
+
+    <input type="text" name="inlognaam" placeholder="email" required value=> <br>
+    <input type="password" name="wachtwoord" placeholder="wachtwoord" required value=>
+    <input type="submit" name="knop" value="verstuur">
+    <?php
+
+
+    if (isset($_POST['knop'])){
+
+        function checkCombo ($name, $password){
+
+            $email = array('piet@worldonline.nl', 'klaas@carpets.nl', 'truushendriks@wegweg.nl');
+            $wachtwoord = array('doetje123', 'snoepje777', 'arkiearkie201');
+            $combination;
+
+            if((($name == $email[0]) && ($password == $wachtwoord[0]))
+                or (($name == $email[1]) && ($password == $wachtwoord[1]))
+                or (($name == $email[2]) && ($password == $wachtwoord[2]))){
+                echo "Welcome!";
+                $combination = true;
+            } else {
+                echo "Sorry, geen toegang";
+                $combination = false;
+            }
+            return $combination;
+        }
+        echo "<br>".checkCombo($_POST['inlognaam'], $_POST['wachtwoord']);
+    }
+
+    ?>
+</FORM>
+</body>
+</html>
