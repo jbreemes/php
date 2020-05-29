3f31161430a2adc8f332931f717752a6c0df2709
jozua <jbreemes@roc-dev.com>
2020-05-29 12:22:37 +0200

    Opdracht 1.6 niet vergeten

--- Opdarcht 3/Opdracht 1.6.php
+++ Opdarcht 3/Opdracht 1.6.php
@@ -1,1 +1,19 @@
 <?php
+
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+
+$aKappersagenda = array(
+    "9:15" => "Mevr. Pietersen",
+    "9:30" => "Mevr. Willems",
+    "9:45" => "",
+    "10:00" => "Paul van den Broek",
+    "10:15" => "Karel de Meeuw",
+    "10:30" => ""
+);
+
+foreach ($aKappersagenda as $tijd => $afspraak) {
+    if ($afspraak == ""){
+        print("<li>".$tijd."</li>") ;
+        echo $afspraak;
+    }
+}
